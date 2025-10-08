ce38afbc9e877e25dabbb00e1bbe38adc5426d2e
Renz Bete <beterenz@gmail.com>
2025-10-08 19:07:34 +0800

    maka use nag api pero dapat naay internet, contacts nalang mga perds kamo nalang butang sa inyu contacts tas basin naa moy idea sa design gamay pwede rasad mag change

---
+++ config.sample.js
@@ -0,0 +1,7 @@
+// Copy this file to `config.js` and paste your API Ninjas key
+// Get a key from: https://api-ninjas.com/
+
+window.APP_CONFIG = {
+  apiNinjasKey: "REPLACE_WITH_YOUR_API_NINJAS_KEY"
+};
+
